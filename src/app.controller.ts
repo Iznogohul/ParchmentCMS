a0b63c15cb64f31556fa459d33ba992d190e1b5b
Nikolaos Grigoropoulos <nikos.gr.17@gmail.com>
2024-06-09 17:02:43 +0300

    chrore: update to latest packages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from "@nestjs/common";
 
 import { AppService } from "./app.service";
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Information Desk")
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getDevInstructions(): string {
+    return this.appService.getDevInstructions();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getDevInstructions(): string {
     return `<div>Welcome to Parchment CMS API.</div><p>To get started, please refer to the Swagger Documentation <a href="http://localhost:${process.env.PORT}/api-docs">here</a>.</p>`;
   }
 }
